b1ecd1caef4137ffe3665f82622763c8a3a4d0c5
Alex Advena <alex.advena@gmail.com>
2022-06-26 00:01:50 -0600

    CustomerService methods updated

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -2,14 +2,17 @@ package service;
 
 import model.Customer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class CustomerService {
     private String email;
     private  String firstName;
     private  String lastName;
     private  String customerEmail;
+    public List<Customer> customers =new ArrayList<Customer>();
 
     public CustomerService (String email, String firstName, String lastName, String customerEmail){
         this.email = email;
@@ -45,16 +48,25 @@ public class CustomerService {
 
 
     public void addCustomer (String email, String firstName, String lastName){
-
+        customers.add(new Customer(firstName, lastName, email));
 
     }
 
     public Customer getCustomer(String customerEmail) {
-    return null;
-    }
+        for (Customer customer : customers){
+            if (customerEmail.equalsIgnoreCase(customerEmail)){
+                return customer;
+            }
+
+            }
+        return null;
+        }
+
+
 
     public Collection<Customer> getAllCustomers() {
-        return null;
+
+        return customers;
     }
 
 }
