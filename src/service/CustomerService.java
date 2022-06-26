33993ebe9b2fe76a92c2e32eafb442cce1097792
Alex Advena <alex.advena@gmail.com>
2022-06-25 23:44:35 -0600

    CustomerService attributes updated

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -6,9 +6,45 @@ import java.util.Collection;
 import java.util.Collections;
 
 public class CustomerService {
+    private String email;
+    private  String firstName;
+    private  String lastName;
+    private  String customerEmail;
+
+    public CustomerService (String email, String firstName, String lastName, String customerEmail){
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.customerEmail = customerEmail;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getFirstName (){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName (){
+        return lastName;
+    }
+    public void setLastName (String lastName){
+        this.lastName = lastName;
+    }
+    public String getCustomerEmail (){
+        return customerEmail;
+    }
+    public void setCustomerEmail(String customerEmail){
+        this.customerEmail = customerEmail;
+    }
 
 
-        public void addCustomer (String email, String firstName, String lastName){
+    public void addCustomer (String email, String firstName, String lastName){
 
 
     }
